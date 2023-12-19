eea5ea1c08759cb54b8c92268ffc884cf952867e
diwahar-m <msdiwahar5@gmail.com>
2023-12-19 16:19:05 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ const winningIndexes =[[0,1,2],[3,4,5],[6,7,8], [0,3,6],[1,4,7],[2,5,8], [0,4,8]
 
 function App() {
 
-  const [array, setArray] = React.useState(Array(9).fill('')); 
+  const [moves, setMoves] = React.useState([]);
+  const [array, setArray] = React.useState(Array(9).fill(''));
+  const [history, setHistory] = React.useState(null);
   const [xValue, setxValue] = React.useState(false);
   const [winner, setWinner] = React.useState('');
 
@@ -20,7 +22,9 @@ function App() {
   React.useEffect(()=>{
 
       (function result(){
-          console.log(array)
+        console.log(array)
+        console.log(moves);
+        console.log(count.current)
           for(let each of winningIndexes){
             if(array[each[0]] === array[each[1]] && array[each[1]] === array[each[2]]) {
               setWinner(array[each[0]]) ;
@@ -28,12 +32,13 @@ function App() {
             }
           }
           
-          if (count.current === 9 & winner === '') setWinner('Draw');
+          if (count.current === 9 && winner === '') setWinner('Draw');
       })()
     
-  },[array,winner])
+  },[array])
 
   function restart(){
+    setMoves([]);
     setArray(Array(9).fill(''));
     setxValue(false);
     setWinner('');
@@ -41,22 +46,76 @@ function App() {
   }
 
   function handleClick(index){
-    console.log(index)
+
     
-    if (xValue){
-      let newArray = [...array]; 
-      newArray[index] = 'O'
-      setArray(newArray);
-      setxValue(false)
+
+    // if (xValue){
+    //   let newArray = [...array]; 
+    //   newArray[index] = 'O'
+    //   setArray(newArray);
+    //   setxValue(false)
+      
+    // } 
+    // else{
+    //   let newArray = [...array]; 
+    //   newArray[index] = 'X'
+    //   setArray(newArray) ;
+    //   setxValue(true)
+    // } 
+
+    
+
+    if(history){
+      let movesArray = [...moves] ;
+      let slicedArray = movesArray.slice(0,history) ;
+      count.current = slicedArray.length ;
+      setMoves(slicedArray);
+      setArray(slicedArray[slicedArray.length-1].stage) 
+
+      if (xValue){
+        let newArray = [...array]; 
+        newArray[index] = 'O'
+        setArray(newArray);
+        setxValue(false)
+        
+      } 
+      else{
+        let newArray = [...array]; 
+        newArray[index] = 'X'
+        setArray(newArray) ;
+        setxValue(true)
+      } 
+      setHistory(null)
+
+    }else{
+
+
+      if (xValue){
+        let newArray = [...array]; 
+        newArray[index] = 'O'
+        setArray(newArray);
+        setxValue(false)
+        
+      } 
+      else{
+        let newArray = [...array]; 
+        newArray[index] = 'X'
+        setArray(newArray) ;
+        setxValue(true)
+      } 
+
       count.current++ ;
-    } 
-    else{
-      let newArray = [...array]; 
-      newArray[index] = 'X'
-      setArray(newArray) ;
-      setxValue(true)
-      count.current++ ;
-    } 
+      let newObj = { stage : array, roll: count.current}
+      setMoves([...moves, newObj]);
+      
+    }
+  }
+
+  function getSelectedObj(roll){
+    setHistory(roll)
+    const filteredObj = moves.find(each => each.roll === roll)
+    console.log(roll)
+    setArray(filteredObj.stage);
   }
 
   return (
@@ -81,30 +140,45 @@ function App() {
               backgroundColor: 'green'}}> Restart </button>
             </div>
             : 
-            <div className='boxes'>
-              <div className='row' >  
-                <Box value={array[0]}  pressed={()=> handleClick(0)}/>
-                <Box value={array[1]} pressed={()=> handleClick(1)}/>
-                <Box value={array[2]}  pressed={()=> handleClick(2)}/>
-              </div>
-              <div className='row' >
-                <Box value={array[3]} pressed={()=> handleClick(3)}/>
-                <Box value={array[4]} pressed={()=> handleClick(4)}/>
-                <Box value={array[5]} pressed={()=> handleClick(5)}/>
-              </div>
-              <div className='row' >
-                <Box value={array[6]}  pressed={()=> handleClick(6)}/>
-                <Box value={array[7]} pressed={()=> handleClick(7)}/>
-                <Box value={array[8]}pressed={()=> handleClick(8)}/>
-              </div>
-              <div>
-                <button onClick={restart} style={{
-                  width: '160px',
-                  marginTop: '20px',
-                  borderRadius: '10px',
-                  backgroundColor: 'green'}}> Restart </button>
+            <div className='boxContainer'>
+              <div className='boxes'>
+                <div className='row' >  
+                  <Box value={array[0]}  pressed={()=> handleClick(0)}/>
+                  <Box value={array[1]} pressed={()=> handleClick(1)}/>
+                  <Box value={array[2]}  pressed={()=> handleClick(2)}/>
+                </div>
+                <div className='row' >
+                  <Box value={array[3]} pressed={()=> handleClick(3)}/>
+                  <Box value={array[4]} pressed={()=> handleClick(4)}/>
+                  <Box value={array[5]} pressed={()=> handleClick(5)}/>
+                </div>
+                <div className='row' >
+                  <Box value={array[6]}  pressed={()=> handleClick(6)}/>
+                  <Box value={array[7]} pressed={()=> handleClick(7)}/>
+                  <Box value={array[8]}pressed={()=> handleClick(8)}/>
+                </div>
+                <div>
+                  <button onClick={restart} style={{
+                    width: '160px',
+                    marginTop: '20px',
+                    borderRadius: '10px',
+                    backgroundColor: 'green'}}> Restart </button>
+                </div>
+              </div> 
+
+
+              <div className='moveContainer'>
+                {
+                  moves.map(each =>(
+                    <div key={each.roll}>
+                      <button className='move' onClick={()=> getSelectedObj(each.roll)}>{`${each.roll}. Move #${each.roll}`}</button>
+                    </div>
+                  ))
+                }
+                
               </div>
             </div>
+            
           }
 
         </div>
@@ -115,4 +189,10 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
+
+// list = [{'move':[]},  {'move':{}}]
+
+// history is maintained 
+// when button is clicked , it shows the stage. 
+// when went back & click new, history got deleted
